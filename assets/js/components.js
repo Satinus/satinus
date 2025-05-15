4701002e94d0d596addc47cd9661275a73a90369
Juraris <jutarisspa@gmail.com>
2025-05-15 16:09:01 -0400

    ajustes de estilo y js

--- assets/js/components.js
+++ assets/js/components.js
@@ -1,14 +1,17 @@
-// Carga componentes
+// Carga componentes con rutas absolutas
 document.addEventListener('DOMContentLoaded', () => {
+    const basePath = window.location.pathname.includes('/pages/') ? '../' : './';
+    
     // Header
-    fetch('./includes/header.html')
+    fetch(`${basePath}includes/header.html`)
         .then(res => res.text())
         .then(html => {
             document.body.insertAdjacentHTML('afterbegin', html);
+            initMenu(); // Llama a la función del menú si existe
         });
     
     // Footer
-    fetch('./includes/footer.html')
+    fetch(`${basePath}includes/footer.html`)
         .then(res => res.text())
         .then(html => {
             document.body.insertAdjacentHTML('beforeend', html);
