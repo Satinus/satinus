d728ab7813fd8aeb5d12d6db878984dce917c232
Juraris <jutarisspa@gmail.com>
2025-05-15 14:46:45 -0400

    Reset completo

--- assets/js/components.js
+++ assets/js/components.js
@@ -1,15 +1,14 @@
-// assets/js/components.js
+// Carga componentes
 document.addEventListener('DOMContentLoaded', () => {
-    // Cargar header AL INICIO del body
-    fetch('/includes/header.html')
+    // Header
+    fetch('./includes/header.html')
         .then(res => res.text())
         .then(html => {
             document.body.insertAdjacentHTML('afterbegin', html);
-            initMenu(); // Si tienes funciones del menú
         });
     
-    // Cargar footer AL FINAL del body
-    fetch('/includes/footer.html')
+    // Footer
+    fetch('./includes/footer.html')
         .then(res => res.text())
         .then(html => {
             document.body.insertAdjacentHTML('beforeend', html);
